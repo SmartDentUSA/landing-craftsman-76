fa492e8c89843b5652f118fa7965ee3c4d354986
SmartDentUSA <smartdentcadcam@gmail.com>
2026-03-17 10:20:31 -0300

    🚀 Publish: smartdent.com.br/ (LP: 83de3354-56e5-4794-b258-b2828fa12d2f)

--- nav-data.js
+++ nav-data.js
@@ -1,9 +1,9 @@
 /* Smart Dent Navigation Data - Auto-generated */
 window.__NAV_DATA__ = [
   {
-    "name": "#1 - Institucional ",
-    "url": "https://smartdent.com.br",
-    "isHome": true,
+    "name": "Comparativo Scanners intraorais ",
+    "url": "https://smartdent.com.br/comparativo-scanners-intraorais",
+    "isHome": false,
     "brand": null
   }
 ];
