3936938a1eba32ffff5b3128be4abfc8463079c5
SmartDentUSA <smartdentcadcam@gmail.com>
2026-03-17 10:42:49 -0300

    🔄 Republish 2 pages on smartdent.com.br (incremental nav update)

--- nav-data.js
+++ nav-data.js
@@ -8,7 +8,7 @@ window.__NAV_DATA__ = [
   },
   {
     "name": "Comparativo Scanners intraorais ",
-    "url": "https://smartdent.com.br/produtos/comparativo-scanners-intraorais",
+    "url": "https://smartdent.com.br/comparativo-scanners-intraorais",
     "isHome": false,
     "brand": null
   }
@@ -25,8 +25,8 @@ window.__NAV_DATA__ = [
   nav.appendChild(title);
   var links = document.createElement('div');
   links.style.cssText = 'display:flex;flex-wrap:wrap;justify-content:center;gap:8px;';
-   var currentUrl = window.location.href.replace(/\/$/, '');
-   data.forEach(function(item) {
+  var currentUrl = window.location.href.replace(/\/$/, '');
+  data.forEach(function(item) {
     if (item.url.replace(/\/$/, '') === currentUrl) return;
     var a = document.createElement('a');
     a.href = item.url;
