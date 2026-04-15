03cd946b23560b6ab9545660d00a90a9cec98a47
SmartDentUSA <smartdentcadcam@gmail.com>
2026-04-14 23:14:36 -0300

    🚀 Publish: smartdent.com.br/manuais (LP: 6416ca26-6d1c-424a-a6de-ffdc6444b4e4)

--- nav-data.js
+++ nav-data.js
@@ -8,13 +8,7 @@ window.__NAV_DATA__ = [
   },
   {
     "name": "Comparativo Scanners intraorais ",
-    "url": "https://smartdent.com.br/produtos/comparativo-scanners-intraorais",
-    "isHome": false,
-    "brand": null
-  },
-  {
-    "name": "Detalhes do Processo por Fluxo  Placas Miorrelaxantes",
-    "url": "https://smartdent.com.br/manuais",
+    "url": "https://smartdent.com.br/comparativo-scanners-intraorais",
     "isHome": false,
     "brand": null
   },
@@ -26,7 +20,7 @@ window.__NAV_DATA__ = [
   },
   {
     "name": "QRCode",
-    "url": "https://smartdent.com.br/suport-resurces ",
+    "url": "https://smartdent.com.br/support-resources",
     "isHome": false,
     "brand": null
   }
@@ -43,8 +37,8 @@ window.__NAV_DATA__ = [
   nav.appendChild(title);
   var links = document.createElement('div');
   links.style.cssText = 'display:flex;flex-wrap:wrap;justify-content:center;gap:8px;';
-  var currentUrl = window.location.href.replace(/\/$/, '');
-  data.forEach(function(item) {
+   var currentUrl = window.location.href.replace(/\/$/, '');
+   data.forEach(function(item) {
     if (item.url.replace(/\/$/, '') === currentUrl) return;
     var a = document.createElement('a');
     a.href = item.url;
