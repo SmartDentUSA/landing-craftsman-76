62c525d5c063e749ca70d5aa31573c902f1235a3
SmartDentUSA <smartdentcadcam@gmail.com>
2026-05-04 08:49:57 -0300

    🚀 Publish: smartdent.com.br/blog/parametros-flashforge-hunter-smart-dent (LP: 6c019910-0f32-41b2-8efa-ee1ba2283bb9)

--- nav-data.js
+++ nav-data.js
@@ -157,6 +157,12 @@ window.__NAV_DATA__ = [
     "brand": "Smart Dent"
   },
   {
+    "name": "Parâmetros elegoo mars-5 | Smart Dent",
+    "url": "https://smartdent.com.br/blog/parametros-elegoo-mars-5-smart-dent",
+    "isHome": false,
+    "brand": "Smart Dent"
+  },
+  {
     "name": "Parâmetros elegoo saturn-3 | Smart Dent",
     "url": "https://smartdent.com.br/blog/parametros-elegoo-saturn-3-smart-dent",
     "isHome": false,
