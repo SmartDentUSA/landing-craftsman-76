d1ca4f061fdad4034dcd5d9db11c764b03816286
SmartDentUSA <smartdentcadcam@gmail.com>
2026-05-04 08:51:43 -0300

    🚀 Publish: smartdent.com.br/es/blog/tos-apas-aturales-en-esina-ompuesta-nterior (LP: 4524ccf5-180d-4977-8b4c-822d86906d9d)

--- nav-data.js
+++ nav-data.js
@@ -91,6 +91,12 @@ window.__NAV_DATA__ = [
     "brand": "Smart Dent"
   },
   {
+    "name": "Parâmetros anycubic photon-mono-4k | Smart Dent",
+    "url": "https://smartdent.com.br/blog/parametros-anycubic-photon-mono-4k-smart-dent",
+    "isHome": false,
+    "brand": "Smart Dent"
+  },
+  {
     "name": "Parâmetros anycubic photon-mono-m5 | Smart Dent",
     "url": "https://smartdent.com.br/blog/parametros-anycubic-photon-mono-m5-smart-dent",
     "isHome": false,
@@ -350,7 +356,7 @@ window.__NAV_DATA__ = [
   },
   {
     "name": "QRCode",
-    "url": "https://smartdent.com.br/suport-resurces ",
+    "url": "https://smartdent.com.br/support-resources",
     "isHome": false,
     "brand": null
   },
