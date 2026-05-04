c59b5077c2f533d6dfd7e9446e605e4323d5c92c
SmartDentUSA <smartdentcadcam@gmail.com>
2026-05-04 08:53:51 -0300

    🚀 Publish: smartdent.com.br/blog/quais-produtos-smart-dent-usar-para-modelos-odontologicos-digitais (LP: 6d4a376f-8d97-4705-9ab0-aa346704afd0)

--- nav-data.js
+++ nav-data.js
@@ -434,7 +434,7 @@ window.__NAV_DATA__ = [
   },
   {
     "name": "QRCode",
-    "url": "https://smartdent.com.br/support-resources",
+    "url": "https://smartdent.com.br/suport-resurces ",
     "isHome": false,
     "brand": null
   },
@@ -465,6 +465,12 @@ window.__NAV_DATA__ = [
   {
     "name": "Quais produtos Smart Dent usar para ortodontia digital e alinhadores?",
     "url": "https://smartdent.com.br/blog/quais-produtos-smart-dent-usar-para-ortodontia-digital-e-alinhadores",
+    "isHome": false,
+    "brand": "Smart Dent"
+  },
+  {
+    "name": "Quais produtos Smart Dent usar para prótese total digital?",
+    "url": "https://smartdent.com.br/blog/quais-produtos-smart-dent-usar-para-protese-total-digital",
     "isHome": false,
     "brand": "Smart Dent"
   },
