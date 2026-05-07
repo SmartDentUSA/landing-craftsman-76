7db4f3a1a2f8f270dfe476591e7be99da8586407
SmartDentUSA <smartdentcadcam@gmail.com>
2026-05-07 18:38:20 -0300

    🔄 Republish 104 pages on smartdent.com.br (incremental nav update)

--- nav-data.js
+++ nav-data.js
@@ -619,8 +619,8 @@ window.__NAV_DATA__ = [
   nav.appendChild(title);
   var links = document.createElement('div');
   links.style.cssText = 'display:flex;flex-wrap:wrap;justify-content:center;gap:8px;';
-   var currentUrl = window.location.href.replace(/\/$/, '');
-   data.forEach(function(item) {
+  var currentUrl = window.location.href.replace(/\/$/, '');
+  data.forEach(function(item) {
     if (item.url.replace(/\/$/, '') === currentUrl) return;
     var a = document.createElement('a');
     a.href = item.url;
